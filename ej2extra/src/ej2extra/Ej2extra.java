6157b1da5eebb5d61e5f86e9fe2f6e727a4cf4d8
Martin <115161917+Martin13025@users.noreply.github.com>
2024-09-07 22:02:19 -0300

    Update Ej2extra.java

--- ej2extra/src/ej2extra/Ej2extra.java
+++ ej2extra/src/ej2extra/Ej2extra.java
@@ -7,16 +7,11 @@ class Ej2extra {
 	public static void main(String[] args) {
 		String nom;
 		int anac, edad, edf, aa;
-		
 		nom=JOptionPane.showInputDialog("ingrese nombre");
 		anac=Integer.parseInt(JOptionPane.showInputDialog("ingrese ano nacimiento"));
 		aa=Integer.parseInt(JOptionPane.showInputDialog("ingrese ano actual"));	
 		edad=aa-anac;
 		edf=2050-anac;
 		JOptionPane.showMessageDialog(null, nom + " su edad es: " + edad + "\nen el 2050: " + edf);
-		
-		
-
 	}
-
 }
