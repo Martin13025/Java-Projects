a58387c809a7d54b2a6db42f6c2a283337020da9
Martin <115161917+Martin13025@users.noreply.github.com>
2024-09-13 21:47:32 -0300

    Update Juegomoneda.java
    Added some strings

--- Juegomoneda.java
+++ Juegomoneda.java
@@ -1,10 +1,16 @@
 package juegomoneda;
 
 import javax.swing.*; //cambiar por *
-
+import javax.swing.ImageIcon;
 class Juegomoneda {
 	public static void main(String[] args) {
 		int m;
+		String nom;
+		//JOptionpane.showinputdialog(null, "menaje", "titulo", tipo de cuadro, icono, null, null)
+		nom=(String) JOptionPane.showInputDialog(null, "ingrese su nombre", "juego con moneda", JOptionPane.DEFAULT_OPTION,
+			new ImageIcon(Juegomoneda.class.getResource("/img/monedaani.gif")), null, null);
+		
+		
 		m=(int)(Math.random() * 2);
 		if (m==0) {
 			//JOptionPane.showmessagedialog(null, "mensaje", "titulo", tipo de cuadro, icono)
