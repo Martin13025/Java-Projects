eba3591fd3c4dccbfd63de6062ab8f5e04aaf7b2
Martin <115161917+Martin13025@users.noreply.github.com>
2024-09-13 21:39:11 -0300

    Add files via upload

---
+++ Juegomoneda.java
@@ -0,0 +1,22 @@
+package juegomoneda;
+
+import javax.swing.*; //cambiar por *
+
+class Juegomoneda {
+
+	public static void main(String[] args) {
+		int m;
+		m=(int)(Math.random() * 2);
+		if (m==0) {
+			//JOptionPane.showmessagedialog(null, "mensaje", "titulo", tipo de cuadro, icono)
+			JOptionPane.showMessageDialog(null, "tienen parcialito", "juego con moneda", JOptionPane.DEFAULT_OPTION,
+					new ImageIcon(Juegomoneda.class.getResource("/img/cara.png")));
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "el tp tiene nota de concepto", "juego con moneda", JOptionPane.DEFAULT_OPTION,
+					new ImageIcon(Juegomoneda.class.getResource("/img/ceca.png")));
+		}
+
+	}
+
+}
