4191550ff5407133bdb98421d30fc98745a1af55
Martin <115161917+Martin13025@users.noreply.github.com>
2024-10-20 17:09:11 -0300

    Add files via upload

---
+++ Promedios.java
@@ -0,0 +1,22 @@
+package promedios;
+
+import javax.swing.JOptionPane;
+
+class Promedios {
+
+	public static void main(String[] args) {
+		double n1 = 0, n2, p;
+		int ca = 0;
+		for (int i = 1; i <= 5; i++)
+			n1 = Double.parseDouble(JOptionPane.showInputDialog("ingrese nota"));
+			n2 = Double.parseDouble(JOptionPane.showInputDialog("ingrese nota"));
+			p = (n1 + n2) / 2;
+			JOptionPane.showMessageDialog(null, "promedio: " + p);
+			System.out.println("promedio: " + p);
+			if (p >= 4) {
+				ca++;
+			}
+			JOptionPane.showMessageDialog(null, "cant aprobados " + ca);
+	}
+
+}
