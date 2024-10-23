f64b288c777d58b5601482fa5fccbea3d95c2009
Martin <115161917+Martin13025@users.noreply.github.com>
2024-10-22 23:13:26 -0300

    Update and rename Ej1sec.java to Ej1sec.js

---
+++ Ej1sec.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("Ingrese numero"));
+let b = parseInt(prompt("Ingrese numero"));
+let c = parseInt(prompt("Ingrese numero"));
+let d = parseInt(prompt("Ingrese numero"));
+
+let sum = a + b;
+let prod = c * d;
+
+alert("Suma: " + sum + " Producto: " + prod);
+
