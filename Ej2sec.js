e77dec3df3567c0f6d2349acf2d1760084cb4c9c
Martin <115161917+Martin13025@users.noreply.github.com>
2025-05-10 22:25:15 -0300

    Update and rename Ej2sec.java to Ej2sec.js
    redone to js

---
+++ Ej2sec.js
@@ -0,0 +1,14 @@
+let a;
+let b;
+let c;  
+let d;
+let s;
+let p;
+
+a = Number(prompt("Enter number: "));
+b = Number(prompt("Enter number: "));
+c = Number(prompt("Enter number: "));
+d = Number(prompt("Enter number: "));
+s = a + b + c + d;
+p = s / 4;
+alert("The sum is: " + s);
