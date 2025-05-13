3a14767397d82d2a5e7cd3cf3207cceddb3da4a0
Martin <115161917+Martin13025@users.noreply.github.com>
2025-05-12 22:50:27 -0300

    Update and rename Ej3sec.java to Ej3sec.js

---
+++ Ej3sec.js
@@ -0,0 +1,9 @@
+let precio;
+let total;
+let cant;
+
+precio = parseFloat(prompt("Ingrese precio"));
+cant = parseFloat(prompt("Ingrese cantidad"));
+total = precio * cant;
+
+alert("Paga: " + total);
